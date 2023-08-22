5f6d4e369c34861df65c054b22daea8b52662d4f
Chatnoir Miki <cmiki@amono.me>
2023-08-23 07:17:42 +0800

    Course information step9 & separate module

--- part2/courseinfo/components/Total.jsx
+++ part2/courseinfo/components/Total.jsx
@@ -1,4 +1,8 @@
 export const Total = ({ parts }) => {
   const exerciseCount = parts.reduce((p, c) => p + c.exercises, 0);
-  return <p>Number of exercises {exerciseCount}</p>;
+  return (
+    <p>
+      <b>Total of {exerciseCount} exercises</b>
+    </p>
+  );
 };
